56c194173b5d3234a6d4b4dc0cafd56b4c2d72cb
cjzhang1230 <1462588499@qq.com>
2019-08-08 12:07:01 +0800

    私钥,keyStore导入钱包

--- app/src/main/java/com/doughnut/activity/KeyStoreImportActivity.java
+++ app/src/main/java/com/doughnut/activity/KeyStoreImportActivity.java
@@ -1,14 +1,19 @@
 package com.doughnut.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.doughnut.R;
+import com.doughnut.config.Constant;
+import com.doughnut.utils.ViewUtil;
 import com.doughnut.view.TitleBar;
+import com.doughnut.wallet.WalletManager;
 
 public class KeyStoreImportActivity extends BaseActivity implements View.OnClickListener {
 
@@ -16,6 +21,10 @@ public class KeyStoreImportActivity extends BaseActivity implements View.OnClick
 
     private ImageView mImgServiceTerms;
     private TextView mTvServiceTerms;
+
+    private EditText mEKeyStore, mEWalletName, mEWalletPwd;
+
+    private Button mBtnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +51,108 @@ public class KeyStoreImportActivity extends BaseActivity implements View.OnClick
         mTvServiceTerms = findViewById(R.id.tv_service_terms);
         mTvServiceTerms.setText(Html.fromHtml(getString(R.string.content_read_service)));
         mTvServiceTerms.setOnClickListener(this);
+
+        mEKeyStore = findViewById(R.id.keyStore_text);
+        mEWalletName = findViewById(R.id.edt_wallet_name);
+        mEWalletPwd = findViewById(R.id.edt_wallet_pwd);
+
+        mBtnConfirm = findViewById(R.id.btn_confirm);
+        mBtnConfirm.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            // 导入按钮
+            case R.id.btn_confirm:
+                if (paramCheck()) {
+                    String keyStore = mEKeyStore.getText().toString();
+                    String walletName = mEWalletName.getText().toString();
+                    String walletPwd = mEWalletPwd.getText().toString();
+                    // 导入钱包
+                    importWallet(keyStore, walletName, walletPwd);
+                }
+                break;
+            // 勾选框
+            case R.id.img_service_terms:
+                mImgServiceTerms.setSelected(!mImgServiceTerms.isSelected());
+                break;
+            // 跳转服务条款页面
+            case R.id.tv_service_terms:
+                gotoServiceTermPage();
+                break;
+        }
     }
+
+    /**
+     * 导入钱包
+     * @param walletName
+     * @param walletPwd
+     */
+    private void importWallet(final String  keyStore, final String walletName, final String walletPwd) {
+
+        WalletManager walletManager =  WalletManager.getInstance(this);
+        // 导入钱包
+        walletManager.importKeysStore(keyStore, walletName);
+//         获取钱包私钥
+//        walletManager.getPrivateKey(walletPwd,walletName);
+    }
+
+    /**
+     * 前端页面校验
+     * @return
+     */
+    private boolean paramCheck() {
+
+        String keyStore = mEKeyStore.getText().toString();
+        String walletName = mEWalletName.getText().toString();
+        String walletPwd = mEWalletPwd.getText().toString();
+//        String walletPwdRepeat = mEWalletPwdConfirm.getText().toString();
+        boolean readedTerms = mImgServiceTerms.isSelected();
+
+        if (TextUtils.isEmpty(keyStore)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_keyStore), "OK");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(walletName)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_wallet_name), "OK");
+            return false;
+        }
+        if (TextUtils.isEmpty(walletPwd)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_password), "OK");
+            return false;
+        }
+
+//        if (TextUtils.isEmpty(walletPwdRepeat)) {
+//            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_verify_password), "OK");
+//            return false;
+//        }
+//
+//        if (!TextUtils.equals(walletPwdRepeat, walletPwd)) {
+//            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_passwords_unmatch), "OK");
+//            return false;
+//        }
+
+        if (walletPwd.length() < 8) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_short_password), "OK");
+            return false;
+        }
+
+        if (!readedTerms) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_read_service), "OK");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 跳转服务条款页面
+     */
+    private void gotoServiceTermPage() {
+        WebBrowserActivity.startWebBrowserActivity(this, getString(R.string.titleBar_service_terms), Constant.service_term_url);
+    }
+
 
 }
--- app/src/main/java/com/doughnut/activity/PrivateKeyImportActivity.java
+++ app/src/main/java/com/doughnut/activity/PrivateKeyImportActivity.java
@@ -1,14 +1,20 @@
 package com.doughnut.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.doughnut.R;
+import com.doughnut.config.Constant;
+import com.doughnut.utils.ViewUtil;
 import com.doughnut.view.TitleBar;
+import com.doughnut.wallet.WalletManager;
 
 public class PrivateKeyImportActivity extends BaseActivity implements View.OnClickListener {
 
@@ -17,6 +23,10 @@ public class PrivateKeyImportActivity extends BaseActivity implements View.OnCli
     private ImageView mImgServiceTerms;
     private TextView mTvServiceTerms;
 
+    private EditText mEPrivateKey, mEWalletName, mEWalletPwd, mEWalletPwdConfirm;
+
+    private Button mBtnConfirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,9 @@ public class PrivateKeyImportActivity extends BaseActivity implements View.OnCli
         initView();
     }
 
+    /**
+     * 画面初期话
+     */
     private void initView() {
         mTitleBar = findViewById(R.id.title_bar);
         mTitleBar.setLeftDrawable(R.drawable.ic_back);
@@ -40,11 +53,113 @@ public class PrivateKeyImportActivity extends BaseActivity implements View.OnCli
         mTvServiceTerms = findViewById(R.id.tv_service_terms);
         mTvServiceTerms.setText(Html.fromHtml(getString(R.string.content_read_service)));
         mTvServiceTerms.setOnClickListener(this);
+
+        mEPrivateKey = findViewById(R.id.edt_private_key);
+        mEWalletName = findViewById(R.id.edt_wallet_name);
+        mEWalletPwd = findViewById(R.id.edt_wallet_pwd);
+        mEWalletPwdConfirm = findViewById(R.id.edt_wallet_pwd_confirm);
+
+        mBtnConfirm = findViewById(R.id.btn_confirm);
+        mBtnConfirm.setOnClickListener(this);
     }
 
+    /**
+     * 页面点击事件
+     * @param view
+     */
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            // 导入按钮
+            case R.id.btn_confirm:
+                if (paramCheck()) {
+                    String privateKey = mEPrivateKey.getText().toString();
+                    String walletName = mEWalletName.getText().toString();
+                    String walletPwd = mEWalletPwd.getText().toString();
+                    // 导入钱包
+                    importWallet(privateKey, walletName, walletPwd);
+                }
+                break;
+            // 勾选框
+            case R.id.img_service_terms:
+                mImgServiceTerms.setSelected(!mImgServiceTerms.isSelected());
+                break;
+            // 跳转服务条款页面
+            case R.id.tv_service_terms:
+                gotoServiceTermPage();
+                break;
+        }
+    }
 
+    /**
+     * 导入钱包
+     * @param walletName
+     * @param walletPwd
+     */
+    private void importWallet(final String  privateKey, final String walletName, final String walletPwd) {
+
+        WalletManager walletManager =  WalletManager.getInstance(this);
+        // 创建钱包
+        walletManager.importWalletWithKey(walletPwd, privateKey, walletName);
+//         获取钱包私钥
+//        walletManager.getPrivateKey(walletPwd,walletName);
+    }
+
+    /**
+     * 前端页面校验
+     * @return
+     */
+    private boolean paramCheck() {
+
+        String privateKey = mEPrivateKey.getText().toString();
+        String walletName = mEWalletName.getText().toString();
+        String walletPwd = mEWalletPwd.getText().toString();
+        String walletPwdRepeat = mEWalletPwdConfirm.getText().toString();
+        boolean readedTerms = mImgServiceTerms.isSelected();
+
+        if (TextUtils.isEmpty(privateKey)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_private_key), "OK");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(walletName)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_wallet_name), "OK");
+            return false;
+        }
+        if (TextUtils.isEmpty(walletPwd)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_password), "OK");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(walletPwdRepeat)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_verify_password), "OK");
+            return false;
+        }
+
+        if (!TextUtils.equals(walletPwdRepeat, walletPwd)) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_passwords_unmatch), "OK");
+            return false;
+        }
+
+        if (walletPwd.length() < 8) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_short_password), "OK");
+            return false;
+        }
+
+        if (!readedTerms) {
+            ViewUtil.showSysAlertDialog(this, getString(R.string.dialog_content_no_read_service), "OK");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    /**
+     * 跳转服务条款页面
+     */
+    private void gotoServiceTermPage() {
+        WebBrowserActivity.startWebBrowserActivity(this, getString(R.string.titleBar_service_terms), Constant.service_term_url);
     }
 
 
