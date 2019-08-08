a734ac6ec30165e69e0fdbae8f393766eb870834
gwang74 <jhell@qq.com>
2019-08-08 09:24:13 +0800

    交易记录页面逻辑实现

--- app/src/main/java/com/doughnut/activity/TransactionRecordActivity.java
+++ app/src/main/java/com/doughnut/activity/TransactionRecordActivity.java
@@ -7,29 +7,42 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.android.jtblk.client.bean.AccountTx;
+import com.android.jtblk.client.bean.Marker;
+import com.android.jtblk.client.bean.Transactions;
 import com.doughnut.R;
 import com.doughnut.adapter.BaseRecycleAdapter;
 import com.doughnut.adapter.BaseRecyclerViewHolder;
 import com.doughnut.base.BaseWalletUtil;
-import com.doughnut.base.WalletInfoManager;
-import com.doughnut.base.WCallback;
 import com.doughnut.base.TBController;
+import com.doughnut.base.WalletInfoManager;
 import com.doughnut.utils.GsonUtil;
 import com.doughnut.utils.TLog;
 import com.doughnut.utils.Util;
 import com.doughnut.utils.ViewUtil;
 import com.doughnut.view.TitleBar;
+import com.doughnut.wallet.JtServer;
+import com.doughnut.wallet.WalletManager;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
-public class TransactionRecordActivity extends BaseActivity implements BaseRecycleAdapter.OnDataLodingFinish,
-        TitleBar.TitleBarClickListener, SwipeRefreshLayout.OnRefreshListener {
+public class TransactionRecordActivity extends BaseActivity implements
+        TitleBar.TitleBarClickListener {
 
     private final static String TAG = "TransactionRecordActivity";
 
@@ -39,6 +52,8 @@ public class TransactionRecordActivity extends BaseActivity implements BaseRecyc
     private RecyclerView mRecyclerView;
     private TransactionRecordAdapter mAdapter;
     private BaseWalletUtil mWalletUtil;
+    private Marker marker;
+    private List<Transactions> transactions;
 
     private View mEmptyView;
 
@@ -60,27 +75,11 @@ public class TransactionRecordActivity extends BaseActivity implements BaseRecyc
             return;
         }
         if (mAdapter != null) {
-            mAdapter.refresh();
             mSmartRefreshLayout.setEnableRefresh(true);
         }
         mTitleBar.setTitle(WalletInfoManager.getInstance().getWname());
 
 
-    }
-
-    @Override
-    public <K> void onDataLoadingFinish(K params, boolean end, boolean loadmore) {
-        if (!loadmore) {
-            if (end) {
-                if (mAdapter.getLength() <= 0) {
-                    mRecyclerView.setVisibility(View.GONE);
-                    mEmptyView.setVisibility(View.VISIBLE);
-                } else {
-                    mRecyclerView.setVisibility(View.VISIBLE);
-                    mEmptyView.setVisibility(View.GONE);
-                }
-            }
-        }
     }
 
     @Override
@@ -110,36 +109,40 @@ public class TransactionRecordActivity extends BaseActivity implements BaseRecyc
         mTitleBar.setBackgroundColor(getResources().getColor(R.color.common_blue));
         mTitleBar.setTitleBarClickListener(this);
 
-        mEmptyView = findViewById(R.id.empty_view);
-        mEmptyView.setVisibility(View.GONE);
+//        mEmptyView = findViewById(R.id.empty_view);
+//        mEmptyView.setVisibility(View.GONE);
 
-//        mRecyclerView = findViewById(R.id.recyclerview_transaction_record);
         mAdapter = new TransactionRecordAdapter();
-        mAdapter.setDataLoadingListener(this);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(TransactionRecordActivity.this));
+        mRecyclerView = findViewById(R.id.view_recycler);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(mAdapter);
-        mRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+        mSmartRefreshLayout = (SmartRefreshLayout) findViewById(R.id.layout_refresh);
+        mSmartRefreshLayout.autoRefresh();
+        mSmartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE && isReadyForPullEnd()) {
-                    //最后一个可见
-                    mAdapter.loadmore(null);
-                }
+            public void onRefresh(RefreshLayout refreshlayout) {
+                mAdapter.notifyDataSetChanged();
+                refreshlayout.finishRefresh();
+
             }
         });
+        mSmartRefreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+            }
+        });
+
+        JtServer.getInstance().changeServer("wss://s.jingtum.com:5020");
+        AccountTx accountTx = WalletManager.getInstance(TransactionRecordActivity.this).getTansferHishory("jBvrdYc6G437hipoCiEpTwrWSRBS2ahXN6", 10, null);
+        transactions = accountTx.getTransactions();
+        marker = accountTx.getMarker();
+        mSmartRefreshLayout.finishRefresh();
     }
 
     public static void startTransactionRecordActivity(Context context, int from) {
         Intent intent = new Intent(context, TransactionRecordActivity.class);
-        intent.putExtra("From", from);
         intent.addFlags(context instanceof BaseActivity ? 0 : Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
-    }
-
-    @Override
-    public void onRefresh() {
-        mAdapter.refresh();
     }
 
     private boolean isReadyForPullEnd() {
@@ -156,143 +159,106 @@ public class TransactionRecordActivity extends BaseActivity implements BaseRecyc
         return false;
     }
 
-    class TransactionRecordAdapter extends BaseRecycleAdapter<String, TransactionRecordAdapter.TransactionRecordViewHolder> {
+    class TransactionRecordAdapter extends RecyclerView.Adapter<TransactionRecordAdapter.VH> {
 
-        private boolean mHasMore = true;
-        private int mPageIndex = 0;
-        private final static int PAGE_SIZE = 10;
-        private String mMarker; //jt 需要
-
-        private BaseRecyclerViewHolder.ItemClickListener mItemClickListener = new BaseRecyclerViewHolder.ItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                GsonUtil item = getItem(position);
-
-                gotoTransactionDetail(item);
-            }
-        };
-
-        @Override
-        public void loadData(final String params, final boolean loadmore) {
-            if (!loadmore) {
-                mPageIndex = 0;
-                mMarker = "";
-            } else {
-                mPageIndex++;
-            }
-            if (loadmore && !mHasMore) {
-                return;
-            }
-
-            if (mDataLoadingListener != null) {
-                mDataLoadingListener.onDataLoadingFinish(params, false, loadmore);
-            }
-
-            GsonUtil requestParams = new GsonUtil("{}");
-            requestParams.putInt("start", mPageIndex);
-            requestParams.putInt("pagesize", PAGE_SIZE);
-            requestParams.putString("marker", mMarker);
-
-            mWalletUtil.queryTransactionList(requestParams, new WCallback() {
-                @Override
-                public void onGetWResult(int ret, GsonUtil extra) {
-                    if (ret == 0) {
-                        handleTransactioRecordResult(params, loadmore, extra);
-                        mMarker = extra.getString("marker", "");
-                    }
-                    mSmartRefreshLayout.setEnableRefresh(false);
-                }
-            });
-        }
-
-        @Override
-        public TransactionRecordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            TransactionRecordViewHolder holder = new TransactionRecordViewHolder(ViewUtil.inflatView(parent.getContext(),
-                    parent, R.layout.layout_item_transaction, false), mItemClickListener);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(TransactionRecordViewHolder transactionRecordViewHolder, int position) {
-            TLog.e(TAG, "itemcount:" + getItemCount());
-            fillData(transactionRecordViewHolder, getItem(position));
-        }
-
-        private void handleTransactioRecordResult(final String params, final boolean loadmore, GsonUtil json) {
-            TLog.d(TAG, "transaction list:" + json);
-            GsonUtil transactionRecord = json.getArray("data", "[]");
-            if (!loadmore) {
-                //第一页
-                setData(transactionRecord);
-            } else {
-                if (transactionRecord.getLength() > 0) {
-                    addData(transactionRecord);
-                }
-            }
-
-            if (transactionRecord.getLength() < PAGE_SIZE) {
-                //最后一页了
-                mHasMore = false;
-            } else {
-                mHasMore = true;
-            }
-
-            if (mDataLoadingListener != null) {
-                mDataLoadingListener.onDataLoadingFinish(params, true, loadmore);
-            }
-        }
-
-        private void fillData(final TransactionRecordViewHolder holder, final GsonUtil item) {
-            if (item == null || TextUtils.equals(item.toString(), "{}")) {
-                return;
-            }
-
-            double value = item.getDouble("real_value", 0.0f);
-            String toAddress = item.getString("to", "");
-
-            String fromAddress = item.getString("from", "");
-            String currentAddress = WalletInfoManager.getInstance().getWAddress().toLowerCase();
-            boolean in = false;
-            holder.mTvTransactionTime.setText(Util.formatTime(item.getLong("timeStamp", 0l)));
-            String label = "";
-            if (TextUtils.equals(currentAddress, fromAddress)) {
-                label = "-";
-                in = false;
-            }
-            if (TextUtils.equals(currentAddress, toAddress)) {
-                label = "+";
-                in = true;
-            }
-            if (in) {
-                holder.mImgIcon.setImageResource(R.drawable.ic_transaction_in);
-                holder.mTvTransactionAddress.setText(fromAddress);
-                holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_blue));
-            } else {
-                holder.mTvTransactionAddress.setText(toAddress);
-                holder.mImgIcon.setImageResource(R.drawable.ic_transaction_out);
-                holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_red));
-            }
-
-            holder.mTvTransactionCount.setText(label + value + item.getString("tokenSymbol", ""));
-        }
-
-        private void gotoTransactionDetail(GsonUtil json) {
-            TransactionDetailsActivity.startTransactionDetailActivity(TransactionRecordActivity.this, json);
-        }
-
-        class TransactionRecordViewHolder extends BaseRecyclerViewHolder {
+        class VH extends RecyclerView.ViewHolder {
             ImageView mImgIcon;
             TextView mTvTransactionAddress;
             TextView mTvTransactionTime;
             TextView mTvTransactionCount;
 
-            public TransactionRecordViewHolder(View itemView, ItemClickListener itemClickListener) {
-                super(itemView, itemClickListener);
+            public VH(View v) {
+                super(v);
                 mImgIcon = itemView.findViewById(R.id.img_icon);
                 mTvTransactionAddress = itemView.findViewById(R.id.tv_transaction_address);
                 mTvTransactionTime = itemView.findViewById(R.id.tv_transaction_time);
                 mTvTransactionCount = itemView.findViewById(R.id.tv_transaction_count);
             }
+        }
+
+        @Override
+        public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = ViewUtil.inflatView(parent.getContext(), parent, R.layout.layout_item_transaction, false);
+            return new VH(v);
+
+        }
+
+        @Override
+        public void onBindViewHolder(VH holder, int position) {
+            if (transactions == null) {
+                return;
+            }
+            Transactions tr = transactions.get(position);
+            JSONObject gets;
+            JSONObject pays;
+            switch (tr.getType()) {
+                case "sent":
+                    holder.mImgIcon.setImageResource(R.drawable.ic_send);
+                    holder.mTvTransactionAddress.setText(tr.getCounterparty());
+                    holder.mTvTransactionCount.setText("-" + tr.getAmount().getValue() + " " + tr.getAmount().getCurrency());
+                    holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_red));
+                    break;
+                case "received":
+                    holder.mImgIcon.setImageResource(R.drawable.ic_reciver);
+                    holder.mTvTransactionAddress.setText(tr.getCounterparty());
+                    holder.mTvTransactionCount.setText("+" + tr.getAmount().getValue() + " " + tr.getAmount().getCurrency());
+                    holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_blue));
+                    break;
+                case "trusted":
+                    break;
+                case "trusting":
+                    break;
+                case "convert":
+                    break;
+                case "offernew":
+                    //Transaction.flags.OfferCreate.Sell 是一个常量
+                    holder.mImgIcon.setImageResource(R.drawable.ic_transaction_out);
+                    pays = tr.getEffects().getJSONObject(0).getJSONObject("paid");
+                    gets = tr.getEffects().getJSONObject(tr.getEffects().size() - 1).getJSONObject("got");
+                    if (pays != null && gets != null) {
+                        holder.mTvTransactionCount.setText(pays.getString("value") + pays.getString("currency") + " -> " + gets.getString("value") + gets.getString("currency"));
+                    } else {
+                        holder.mTvTransactionCount.setText(tr.getGets().getValue() + tr.getGets().getCurrency() + " -> " + tr.getPays().getValue() + tr.getPays().getCurrency());
+                    }
+                    holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_green));
+                    break;
+                case "offercancel":
+                    holder.mImgIcon.setImageResource(R.drawable.ic_transfer);
+                    holder.mTvTransactionCount.setText(tr.getGets().getValue() + tr.getGets().getCurrency() + " -> " + tr.getPays().getValue() + tr.getPays().getCurrency());
+                    holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_green));
+                    break;
+                case "offereffect":
+                    holder.mImgIcon.setImageResource(R.drawable.ic_token_normal);
+                    holder.mTvTransactionAddress.setText(tr.getEffects().getJSONObject(0).getJSONObject("counterparty").getString("account"));
+                    pays = tr.getEffects().getJSONObject(0).getJSONObject("paid");
+                    gets = tr.getEffects().getJSONObject(0).getJSONObject("got");
+                    holder.mTvTransactionCount.setText(pays.getString("value") + pays.getString("currency") + " -> " + gets.getString("value") + gets.getString("currency"));
+                    holder.mTvTransactionCount.setTextColor(getResources().getColor(R.color.common_green));
+                    break;
+                case "relationset":
+                    break;
+                case "relationdel":
+                    break;
+                case "configcontract":
+                    break;
+                default:
+                    // TODO parse other type
+                    break;
+            }
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = new Date(tr.getDate().longValue() * 1000);
+            String sim = formatter.format(date);
+            holder.mTvTransactionTime.setText(sim);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemCount() {
+            return transactions.size();
         }
     }
 
