8423ffb844daf56ba9de9e29674c0d78ba2f9f2b
JackHuaBin <jhell@qq.com>
2019-10-28 11:03:29 +0800

    界面显示控制逻辑调整

--- app/src/main/java/com/doughnut/activity/AddCurrencyActivity.java
+++ app/src/main/java/com/doughnut/activity/AddCurrencyActivity.java
@@ -245,13 +245,8 @@ public class AddCurrencyActivity extends BaseActivity implements TitleBar.TitleB
             holder.mImgIcon.setImageResource(tr.getImage());
             String tokenName = tr.getName();
             holder.token = tokenName;
-
-            holder.mTvTokenName.setText(TextUtils.isEmpty(tokenName) ? ""
-                    : (tokenName.length() <= 12 ? tokenName
-                    : new StringBuilder(tokenName)
-                    .replace(5, tokenName.length() - 4, "***")
-                    .toString()));
-            //ViewUtil.EllipsisTextView(holder.mTvTokenName);
+            holder.mTvTokenName.setText(tokenName);
+            ViewUtil.EllipsisTextView(holder.mTvTokenName);
             if (!mIsSingle) {
                 holder.chk_select.setChecked(tr.getIsSelect());
             }
