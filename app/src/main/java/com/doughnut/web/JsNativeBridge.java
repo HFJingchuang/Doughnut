fec82c830282c442fcb8e9a4f7d2a02e85922703
JackHuaBin <jhell@qq.com>
2019-10-28 16:33:13 +0800

    webview加载框架封装、Js调用原生接口开发

---
+++ app/src/main/java/com/doughnut/web/JsNativeBridge.java
@@ -0,0 +1,161 @@
+package com.doughnut.web;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.webkit.JavascriptInterface;
+
+import com.android.jtblk.client.bean.Marker;
+import com.doughnut.wallet.ICallBack;
+import com.doughnut.wallet.WalletManager;
+import com.jccdex.rpc.base.JCallback;
+import com.just.agentweb.AgentWeb;
+
+/**
+ * JS调用原生接口类
+ */
+public class JsNativeBridge {
+    private AgentWeb agent;
+    private Context context;
+    private WalletManager mWalletManager;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+
+    public JsNativeBridge(AgentWeb agent, Context context) {
+        this.agent = agent;
+        this.context = context;
+        this.mWalletManager = WalletManager.getInstance(context);
+    }
+
+    @JavascriptInterface
+    public void createWallet(String password, String name, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.createWallet(password, name, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void deleteWallet(String address) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.deleteWallet(address);
+            }
+        });
+    }
+
+    //TODO 后续需要定义如何传递Bitmap给js
+    @JavascriptInterface
+    public Bitmap exportWalletWithQR(String address, int widthAndHeight, int color) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.exportWalletWithQR(address, widthAndHeight, color);
+            }
+        });
+        return null;
+    }
+
+    @JavascriptInterface
+    public void importWalletWithKey(String password, String privateKey, String name, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.importWalletWithKey(password, privateKey, name, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void importKeysStore(String keyStore, String password, String name, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.importKeysStore(keyStore, password, name, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getPrivateKey(String password, String keyStore, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getPrivateKey(password, keyStore, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void transfer(String privateKey, String from, String to, String token, String issuer, String value, String fee, String memo, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.transfer(privateKey, from, to, token, issuer, value, fee, memo, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getTransferHistory(String address, Integer limit, Marker marker, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getTransferHistory(address, limit, marker, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getBalance(String address, boolean isDisposable, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getBalance(address, isDisposable, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getSWTBalance(String address, ICallBack callBack) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getSWTBalance(address, callBack);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getTokenPrice(String base, JCallback jCallback) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getTokenPrice(base, jCallback);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getAllTokenPrice(JCallback jCallback) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getAllTokenPrice(jCallback);
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void getAllTokens() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                mWalletManager.getAllTokens();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/doughnut/web/WebActivity.java
@@ -0,0 +1,110 @@
+package com.doughnut.web;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+import com.doughnut.R;
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.DefaultWebClient;
+import com.just.agentweb.WebChromeClient;
+import com.just.agentweb.WebViewClient;
+
+public class WebActivity extends AppCompatActivity {
+
+    public static final String LOAD_URL = "load_url";
+    protected AgentWeb mAgentWeb;
+    private LinearLayout mLinearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        mLinearLayout = (LinearLayout) this.findViewById(R.id.container);
+        mAgentWeb = AgentWeb.with(this)
+                .setAgentWebParent(mLinearLayout, new LinearLayout.LayoutParams(-1, -1))
+                .useDefaultIndicator()
+                .setWebChromeClient(mWebChromeClient)
+                .setWebViewClient(mWebViewClient)
+                .setMainFrameErrorView(R.layout.agentweb_error_page, -1)
+                .setSecurityType(AgentWeb.SecurityType.STRICT_CHECK)
+                .setOpenOtherPageWays(DefaultWebClient.OpenOtherPageWays.ASK)
+                .interceptUnkownUrl()
+                .createAgentWeb()
+                .ready()
+                .go(getUrl());
+
+        mAgentWeb.getJsInterfaceHolder().addJavaObject("JsNativeBridge", new JsNativeBridge(mAgentWeb, this));
+    }
+
+    private com.just.agentweb.WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+        }
+    };
+
+    private com.just.agentweb.WebChromeClient mWebChromeClient = new WebChromeClient() {
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+        }
+    };
+
+    //用户需要在打开WebActivity时传递对应的url
+    public String getUrl() {
+        Intent intent = getIntent();
+        String load_url = "";
+        if (intent != null) {
+            load_url = intent.getStringExtra(LOAD_URL);
+        }
+        return load_url;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (mAgentWeb.handleKeyEvent(keyCode, event)) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onPause() {
+        mAgentWeb.getWebLifeCycle().onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mAgentWeb.getWebLifeCycle().onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i("Info", "onResult:" + requestCode + " onResult:" + resultCode);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAgentWeb.getWebLifeCycle().onDestroy();
+    }
+}
